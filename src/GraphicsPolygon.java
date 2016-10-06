e0c6c612b1883ff928ac1ae31c5d8ee29059a498
jillHuang7 <yhuan437@mail.ccsf.edu>
2016-10-06 09:51:11 -0700

    Update GraphicsPolygon.java

--- src/GraphicsPolygon.java
+++ src/GraphicsPolygon.java
@@ -4,15 +4,20 @@ import java.awt.Polygon;
 /**
  * A class for polygon shape, extend GraphicsBaseclass, and override GraphicShape interface as well.
  *
+ * @author Karl Nicholas
+ * @author Calvin Lee
+ * @author Yu-Hsiang Huang
  */
 public class GraphicsPolygon extends GraphicsBaseclass {
 	private final GraphicShape[] vertixPolygons;
 	private final Polygon polygon;
 
+	//constructor for GraphicsPolygon class, invoke the instance data from ShapeContainer
 	public GraphicsPolygon(boolean change, Color color, int sides, Point center, int radius, double rotation, double recursionFactor, int counter) {
 		super(change, color, sides, center, radius, rotation, recursionFactor, counter);
 		vertixPolygons = new GraphicsPolygon[sides];
-
+		
+		//calculation for polygon shape
 		double slice = (2 * Math.PI) / ((double) sides);
 		int[] xVertices = new int[sides];
 		int[] yVertices = new int[sides];
@@ -24,6 +29,7 @@ public class GraphicsPolygon extends GraphicsBaseclass {
 		polygon = new Polygon(xVertices, yVertices, sides);
 	}
 
+	//override paintComponent interface for access the methods inside
 	@Override
 	public void paintComponent(Graphics2D g) {
 		g.setColor(this.color);
